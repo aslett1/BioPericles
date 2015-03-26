e403f8bac95f5ad5809eb8c9088111a386fcc0f6
Ben Taylor <bt5@sanger.ac.uk>
2015-03-26 14:11:02 +0000

    Initial commit with test data etc.

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+import multiprocessing
+
+def readme():
+  with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    return f.read()
+
+setup(name='biopericles',
+      version='0.0.1',
+      description='TBC',
+      long_description=readme(),
+      url='https://github.com/sanger-pathogens/BioPericles',
+      author='Ben Taylor, Andrew J. Page',
+      author_email='ben.taylor@sanger.ac.uk, ap13@sanger.ac.uk',
+      scripts=[],
+      include_package_data=True,
+      install_requires=[
+      ],
+      test_suite='nose.collector',
+      tests_require=[
+        'nose',
+        'mock'
+      ],
+      license='GPLv3',
+      packages=find_packages(),
+      classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ]
+)
