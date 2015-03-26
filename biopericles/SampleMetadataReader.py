06c7481d50a9a74ee5d5736dddc559e2c1bb9e16
Ben Taylor <bt5@sanger.ac.uk>
2015-03-26 15:56:53 +0000

    Turn parsed metadata lines into maps
    maps are between clusters and sequences and vice versa

--- biopericles/SampleMetadataReader.py
+++ biopericles/SampleMetadataReader.py
@@ -1,5 +1,8 @@
 import os
 
+class BadMetadataException(ValueError):
+  pass
+
 class SampleMetadataReader(object):
 
   def __init__(self, filename):
@@ -11,3 +14,17 @@ class SampleMetadataReader(object):
 
   def parse_line(self, line_data, sample_name_idx, cluster_name_idx):
     return (line_data[sample_name_idx], line_data[cluster_name_idx])
+
+  def create_cluster_sample_map(self, sample_cluster_tuples):
+    cluster_sample_map = {}
+    for sample, cluster in sample_cluster_tuples:
+      cluster_sample_map.setdefault(cluster, []).append(sample)
+    unique_cluster_sample_map = {k: sorted(list(set(v))) for k,v in cluster_sample_map.items()}
+    return unique_cluster_sample_map
+
+  def create_sample_cluster_map(self, sample_cluster_tuples):
+    deduplicated_sample_cluster_tuples = list(set(sample_cluster_tuples))
+    samples = list(set([sample for sample, cluster in deduplicated_sample_cluster_tuples]))
+    if len(samples) != len(deduplicated_sample_cluster_tuples):
+      raise BadMetadataException("Each sample should only be attributed to one cluster")
+    return { sample: cluster for sample, cluster in deduplicated_sample_cluster_tuples }
--- biopericles/tests/SampleMetadataReader_test.py
+++ biopericles/tests/SampleMetadataReader_test.py
@@ -1,7 +1,7 @@
 import unittest
 from mock import patch
 
-from biopericles.SampleMetadataReader import SampleMetadataReader
+from biopericles.SampleMetadataReader import SampleMetadataReader, BadMetadataException
 
 class TestSampleMetadataReader(unittest.TestCase):
 
@@ -18,3 +18,43 @@ class TestSampleMetadataReader(unittest.TestCase):
     self.assertEqual(reader.parse_line(line, 1, 0), ('sequence1', 'clusterA'))
 
     self.assertRaises(IndexError, reader.parse_line, line, 10, 0)
+
+  @patch('biopericles.SampleMetadataReader.os')
+  def test_create_cluster_sample_map(self, os_mock):
+    os_mock.path.isfile.return_value = True
+    reader = SampleMetadataReader('')
+
+    output = reader.create_cluster_sample_map([ ('seq_1', 'cluster_A'),
+                                                ('seq_1', 'cluster_A'),
+                                                ('seq_2', 'cluster_B'),
+                                                ('seq_3', 'cluster_B'),
+                                                ('seq_4', 'cluster_C') ])
+    expected_output = {
+                        'cluster_A': ['seq_1'],
+                        'cluster_B': ['seq_2', 'seq_3'],
+                        'cluster_C': ['seq_4']
+                      }
+
+    self.assertEqual(output, expected_output)
+
+  @patch('biopericles.SampleMetadataReader.os')
+  def test_create_sample_cluster_map(self, os_mock):
+    os_mock.path.isfile.return_value = True
+    reader = SampleMetadataReader('')
+
+    output = reader.create_sample_cluster_map([ ('seq_1', 'cluster_A'),
+                                                ('seq_1', 'cluster_A'),
+                                                ('seq_2', 'cluster_B'),
+                                                ('seq_3', 'cluster_B'),
+                                                ('seq_4', 'cluster_C') ])
+    expected_output = {
+                        'seq_1': 'cluster_A',
+                        'seq_2': 'cluster_B',
+                        'seq_3': 'cluster_B',
+                        'seq_4': 'cluster_C'
+                      }
+
+    self.assertEqual(output, expected_output)
+
+    sample_cluster_tuples = [('seq_1', 'cluster_A'), ('seq_1', 'cluster_B')]
+    self.assertRaises(BadMetadataException, reader.create_sample_cluster_map, sample_cluster_tuples)
