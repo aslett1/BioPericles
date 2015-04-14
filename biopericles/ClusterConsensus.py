4ab4f759fcaf29b8049abde7ee392e8faf1e4ea2
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-04-14 13:22:49 +0000

    Privatise methods; write output to file

--- biopericles/ClusterConsensus.py
+++ biopericles/ClusterConsensus.py
@@ -1,36 +1,66 @@
-import Bio.SeqIO
+import os
+import random
 
-class ClusterFastaFile(object):
+import Bio.SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+class ClusterConsensus(object):
 
   def __init__(self):
-    self.sequences = None
+    self.sequences = None # a SeqRecord generator or list
+    self.output_file = None
+    self.cluster_name = None
+
+  def load_fasta_file(self, cluster_file):
+    self.sequences = Bio.SeqIO.parse(cluster_file, 'fasta')
+    if not self.cluster_name:
+      self.cluster_name = self._get_cluster_name()
 
   def get_consensus(self):
-    return self.calculate_consensus(self.sequences)
+    return self._calculate_consensus(self.sequences)
 
-  def load_file(self, cluster_file):
-    self.sequences = Bio.SeqIO.parse(cluster_file, 'fasta')
+  def create_consensus_sequence(self):
+    consensus = self._calculate_consensus(self.sequences)
+    seq = Seq(consensus)
+    name = self._get_cluster_name()
+    return SeqRecord(seq, description='', id=name)
 
-  def calculate_consensus(self, sequence_generator):
+  def write_consensus(self):
+    consensus_sequence = self.create_consensus_sequence()
+    Bio.SeqIO.write(consensus_sequence, self.output_file, 'fasta')
+
+
+  def _calculate_consensus(self, sequence_generator):
     consensus = sequence_generator.next()
     for sequence in sequence_generator:
-      consensus = self.get_pair_consensus(consensus, sequence)
+      consensus = self._get_pair_consensus(consensus, sequence)
     return consensus
 
-  def get_pair_consensus(self, sequence_1, sequence_2):
+  def _get_pair_consensus(self, sequence_1, sequence_2):
     if len(sequence_1) != len(sequence_2):
       message = """\
 Cannot evaluate consensus between sequences of differing lengths.
 Sequence 1 was {} characters long, Sequence 2 was {} characters long.
 """
       raise ValueError(message.format(len(sequence_1), len(sequence_2)))
-    consensus = map(self.compare_nucleotides, *(sequence_1,sequence_2))
+    consensus = map(self._compare_nucleotides, *(sequence_1,sequence_2))
     return ''.join(consensus)
 
-  def compare_nucleotides(self, n1, n2):
+  def _compare_nucleotides(self, n1, n2):
     if n1 == '-' or n2 == '-':
       return 'N'
     elif n1 == n2:
       return n1
     else:
       return 'N'
+
+  def _get_cluster_name(self):
+    if self.cluster_name:
+      return self.cluster_name
+    try:
+      filename = self.output_file.name
+      name = os.path.splitext(os.path.basename(filename))[0]
+    except AttributeError:
+      name = "cluster_%s" % random.randrange(10000,100000)
+    return name
--- biopericles/tests/ClusterConsensus_test.py
+++ biopericles/tests/ClusterConsensus_test.py
@@ -1,11 +1,10 @@
 import unittest
-import os
 
 from StringIO import StringIO
 from mock import patch, MagicMock
 from unittest import skip
 
-from biopericles.ClusterFastaFile import ClusterFastaFile
+from biopericles.ClusterConsensus import ClusterConsensus
 
 class GeneratorMock(object):
   def __init__(self, sequences):
@@ -21,48 +20,48 @@ class GeneratorMock(object):
     self.call_count += 1
     return self.sequences.next()
 
-class TestClusterFastaFile(unittest.TestCase):
+class TestClusterConsensus(unittest.TestCase):
   def test_get_pair_consensus(self):
-    cluster = ClusterFastaFile()
+    cluster = ClusterConsensus()
 
-    self.assertRaises(ValueError, cluster.get_pair_consensus, 'A', 'AG')
-    self.assertEqual(cluster.get_pair_consensus('AG', 'AG'), 'AG')
-    self.assertEqual(cluster.get_pair_consensus('AG', 'AC'), 'AN')
-    self.assertEqual(cluster.get_pair_consensus('AGT', 'ACT'), 'ANT')
-    self.assertEqual(cluster.get_pair_consensus('-GT', 'ACT'), 'NNT')
-    self.assertEqual(cluster.get_pair_consensus('-GT', '-CT'), 'NNT')
+    self.assertRaises(ValueError, cluster._get_pair_consensus, 'A', 'AG')
+    self.assertEqual(cluster._get_pair_consensus('AG', 'AG'), 'AG')
+    self.assertEqual(cluster._get_pair_consensus('AG', 'AC'), 'AN')
+    self.assertEqual(cluster._get_pair_consensus('AGT', 'ACT'), 'ANT')
+    self.assertEqual(cluster._get_pair_consensus('-GT', 'ACT'), 'NNT')
+    self.assertEqual(cluster._get_pair_consensus('-GT', '-CT'), 'NNT')
 
   def test_compare_nucleotides(self):
-    cluster = ClusterFastaFile()
+    cluster = ClusterConsensus()
 
-    self.assertEqual(cluster.compare_nucleotides('A', 'A'), 'A')
-    self.assertEqual(cluster.compare_nucleotides('A', 'T'), 'N')
-    self.assertEqual(cluster.compare_nucleotides('A', 'N'), 'N')
-    self.assertEqual(cluster.compare_nucleotides('N', 'N'), 'N')
-    self.assertEqual(cluster.compare_nucleotides('A', '-'), 'N')
-    self.assertEqual(cluster.compare_nucleotides('-', '-'), 'N')
+    self.assertEqual(cluster._compare_nucleotides('A', 'A'), 'A')
+    self.assertEqual(cluster._compare_nucleotides('A', 'T'), 'N')
+    self.assertEqual(cluster._compare_nucleotides('A', 'N'), 'N')
+    self.assertEqual(cluster._compare_nucleotides('N', 'N'), 'N')
+    self.assertEqual(cluster._compare_nucleotides('A', '-'), 'N')
+    self.assertEqual(cluster._compare_nucleotides('-', '-'), 'N')
 
   def test_calculate_consensus(self):
-    cluster = ClusterFastaFile()
+    cluster = ClusterConsensus()
 
     sequence_generator = (s for s in ['AG', 'AG', 'A'])
-    self.assertRaises(ValueError, cluster.calculate_consensus, sequence_generator)
+    self.assertRaises(ValueError, cluster._calculate_consensus, sequence_generator)
 
     sequence_generator = (s for s in ['AG', 'AG', 'AG'])
-    self.assertEqual(cluster.calculate_consensus(sequence_generator), 'AG')
+    self.assertEqual(cluster._calculate_consensus(sequence_generator), 'AG')
 
     sequence_generator = (s for s in ['AG', 'AG', 'TG'])
-    self.assertEqual(cluster.calculate_consensus(sequence_generator), 'NG')
+    self.assertEqual(cluster._calculate_consensus(sequence_generator), 'NG')
 
     sequence_generator = (s for s in ['AG', 'AT', 'TG'])
-    self.assertEqual(cluster.calculate_consensus(sequence_generator), 'NN')
+    self.assertEqual(cluster._calculate_consensus(sequence_generator), 'NN')
 
     sequence_generator = (s for s in ['AG', 'A-', 'AG'])
-    self.assertEqual(cluster.calculate_consensus(sequence_generator), 'AN')
+    self.assertEqual(cluster._calculate_consensus(sequence_generator), 'AN')
 
-  @patch('biopericles.ClusterFastaFile.Bio.SeqIO')
-  def test_load_file(self, seqio_mock):
-    cluster = ClusterFastaFile()
+  @patch('biopericles.ClusterConsensus.Bio.SeqIO')
+  def test_load_fasta_file(self, seqio_mock):
+    cluster = ClusterConsensus()
 
     fake_file = StringIO("Some sequences")
 
@@ -70,7 +69,7 @@ class TestClusterFastaFile(unittest.TestCase):
 
     seqio_mock.parse.return_value = fake_sequence_generator
 
-    cluster.load_file(fake_file)
+    cluster.load_fasta_file(fake_file)
     seqio_mock.parse.assert_called_once_with(fake_file, 'fasta')
     self.assertEqual(cluster.sequences, fake_sequence_generator)
     self.assertEqual(fake_sequence_generator.call_count, 0)
@@ -78,10 +77,34 @@ class TestClusterFastaFile(unittest.TestCase):
     self.assertEqual(cluster.get_consensus(), 'AG')
     self.assertEqual(fake_sequence_generator.call_count, 3)
 
-  def test_actually_load_a_file(self):
-    cluster = ClusterFastaFile()
-    this_dir = os.path.dirname(__file__)
-    test_file_path = os.path.join(this_dir, 'data', 'cluster_A_multifasta.aln')
-    with open(test_file_path, 'r') as test_file:
-      cluster.load_file(test_file)
-      self.assertEqual(cluster.get_consensus(), 'ANNAACAAAANN')
+  @patch('biopericles.ClusterConsensus.random.randrange')
+  def test_get_cluster_name(self, random_mock):
+    cluster = ClusterConsensus()
+
+    cluster.output_file = StringIO()
+    random_mock.return_value = 10000
+    self.assertEqual(cluster._get_cluster_name(), 'cluster_10000')
+    
+    cluster.output_file = MagicMock()
+    cluster.output_file.name = 'foo/bar.baz.mfa'
+    self.assertEqual(cluster._get_cluster_name(), 'bar.baz')
+
+    cluster.cluster_name = 'foo'
+    self.assertEqual(cluster._get_cluster_name(), 'foo')
+
+  def test_write_consensus(self):
+    cluster = ClusterConsensus()
+
+    cluster.output_file = StringIO()
+    cluster.cluster_name = 'cluster_1000'
+    cluster._calculate_consensus = MagicMock(return_value='NNANTGUNCN')
+
+    cluster.write_consensus()
+
+    expected_results = """\
+>cluster_1000
+NNANTGUNCN
+"""
+    cluster.output_file.seek(0)
+    self.assertEqual(cluster.output_file.read(), expected_results)
+
